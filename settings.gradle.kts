154c41270c69d6777eea9ef425f067dca1ec5ff2
bausabunmandi <bausabunmandi@gmail.com>
2025-04-02 16:59:41 +0700

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,19 @@ rootProject.name = "CloudstreamPlugins"
 // This file sets what projects are included. Every time you add a new project, you must add it
 // to the includes below.
 
+val disabled = listOf<String>()
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
+    }
+}
+
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
+
 // Plugins are included like this
-include(
-    "ExampleProvider"
-)
+// include(
+//     "ExampleProvider"
+// )
