f0cf787211ae8b31bc87ff649f7b403483257ed8
bausabunmandi <bausabunmandi@gmail.com>
2025-04-02 23:15:05 +0700

    test

--- avtub/src/main/kotlin/com/sabunmandi/Extractors.kt
+++ avtub/src/main/kotlin/com/sabunmandi/Extractors.kt
@@ -3,7 +3,13 @@ package com.sabunmandi
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.utils.JsUnpacker
 import com.lagradost.cloudstream3.extractors.FileMoonSx
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
+import com.lagradost.cloudstream3.extractors.Filesim
 
-class FileMoonTo : FileMoonSx() {
+class FileMoonTo : Filesim() {
     override var mainUrl = "https://filemoon.to"
+    override val name = "FileMoonTo"
 }
