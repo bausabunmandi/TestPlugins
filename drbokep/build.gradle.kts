725e11c06615aadd38bb789393a1171b793f377a
bausabunmandi <bausabunmandi@gmail.com>
2025-09-28 16:24:49 +0700

    fix type

--- drbokep/build.gradle.kts
+++ drbokep/build.gradle.kts
@@ -15,7 +15,7 @@ cloudstream {
     /** Status int as the following: 0: Down 1: Ok 2: Slow 3: Beta only */
     status = 1
 
-    tvTypes = listOf("NSFW")
+    tvTypes = listOf("Movie")
 
     requiresResources = true
     language = "id"
--- sebokep/build.gradle.kts
+++ sebokep/build.gradle.kts
@@ -15,10 +15,10 @@ cloudstream {
     /** Status int as the following: 0: Down 1: Ok 2: Slow 3: Beta only */
     status = 1
 
-    tvTypes = listOf("NSFW")
+    tvTypes = listOf("Movie")
 
     requiresResources = true
-    language = "en"
+    language = "id"
 
     // random cc logo i found
     iconUrl = "https://www.google.com/s2/favicons?domain=sebokep.com&sz=%size%"
